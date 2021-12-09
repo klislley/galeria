dcc6eea650e82a96c3cbd925f2d13474b25123ea
Klislley Costa <klislley@outlook.com.br>
2021-12-08 21:48:59 -0400

    atualização_1

---
+++ js/main.js
@@ -0,0 +1,45 @@
+class PhotoGallery {
+  constructor() {
+    this.API_KEY = "563492ad6f9170000100000149fe7881c54e40e2911701b47446c8da";
+    this.galleryDvl = document.querySelector(".gallery");
+    this.searchForm = document.querySelector("header form");
+    this.loadMore = document.querySelector(".load-more");
+    this.eventHandle();
+  }
+  eventHandle() {
+    document.addEventListener("DOMContentLoaded", () => {
+      this.getImg();
+    });
+  }
+  async getImg() {
+    const baseURL = "https://api.pexels.com/v1/curated?per_page=12";
+    const data = await this.fetchImages(baseURL);
+    this.GenerateHTML(data.photos);
+    console.log(data);
+  }
+  async fetchImages(baseURL) {
+    const response = await fetch(baseURL, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: this.API_KEY,
+      },
+    });
+    const data = await response.json();
+    return data;
+  }
+  GenerateHTML(photos) {
+    photos.forEach((photo) => {
+      const item = document.createElement("div");
+      item.classList.add("item");
+      item.innerHTML = `
+        <a href="#">
+          <img src="${photo.src.medium}">
+          <h3>${photo.photographer}</h3>
+        </a>
+      `;
+      this.galleryDvl.appendChild(item);
+    });
+  }
+}
+const gallery = new PhotoGallery();
